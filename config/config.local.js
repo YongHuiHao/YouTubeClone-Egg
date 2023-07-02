af4ebf6a335b4fe52ef547f9a70cf6875a4ce866
HaoHao <admin@HaoHaodeMacBook-Pro.local>
2023-07-02 19:20:18 +0800

    Refresh .gitignore

--- config/config.local.js
+++
@@ -1,9 +0,0 @@
-/* 
-  针对本地的配置文件
-*/
-
-const secret = require('./secret')
-
-module.exports = {
-  ...secret
-}
--- config/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  vod: {
-    accessKeyId: 'LTAI5t7ra37hovjqCEz6T6Rp',
-    accessKeySecret: '1BfQ4kf6qY13194iXqgD8qJ89Rj0QP'
-  }
-}
