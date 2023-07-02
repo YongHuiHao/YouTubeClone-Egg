7e52b6f6d7555e9d32a7c9363ea16533df2bf7c6
HaoHao <admin@HaoHaodeMacBook-Pro.local>
2023-07-02 11:45:40 +0800

    修改生产环境配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   vod: {
-    accessKeyId: process.env.accessKeyId,
-    accessKeySecret: process.env.accessKeySecret
+    accessKeyId: 'LTAI5t7ra37hovjqCEz6T6Rp',
+    accessKeySecret: '1BfQ4kf6qY13194iXqgD8qJ89Rj0QP'
   }
 }
