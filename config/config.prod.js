54c1b48649c15e7f23bc7127518c1d97213a12b5
HaoHao <admin@HaoHaodeMacBook-Pro.local>
2023-07-02 19:16:58 +0800

    prod使用环境变量

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   vod: {
-    accessKeyId: 'LTAI5t7ra37hovjqCEz6T6Rp',
-    accessKeySecret: '1BfQ4kf6qY13194iXqgD8qJ89Rj0QP'
+    accessKeyId: process.env.ACCESSKEYID,
+    accessKeySecret: process.env.ACCESSKEYSECRET
   }
 }
